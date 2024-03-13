fca99da2ac7e7b7e5ef0f1667c7ca92d0cb23a2a
Maksim Gurkov <maksim.gurkov@gmail.com>
2024-03-13 11:52:46 +0300

    Initial Commit

---
+++ TestOMG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestOMG
+//
+//  Created by Максим Гурков on 13.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
